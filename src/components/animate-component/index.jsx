8f4a39c3cf4322109564e64c7eb20da122b7c39b
Maryam Moshtoofar <maryammoshtoofar@gmail.com>
2023-02-27 16:58:29 +0330

    UI:add cards and resume animations with framer

--- src/components/animate-component/index.jsx
+++ src/components/animate-component/index.jsx
@@ -1,15 +1,9 @@
 import { motion, MotionConfig } from "framer-motion";
 
 const Animated = ({ animateConfig, children }) => {
-  const { initial, whileInView } = animateConfig;
-
   return (
     <MotionConfig transition={{ duration: 1 }}>
-      <motion.div
-        initial={initial}
-        whileInView={whileInView}
-        viewport={{ once: true }}
-      >
+      <motion.div {...animateConfig} viewport={{ once: true }}>
         {children}
       </motion.div>
     </MotionConfig>
